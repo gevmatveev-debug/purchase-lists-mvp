06dc2825826f969cf2910e1d3c5de203e979c2dc
gevmatveev-debug <ge.vmatveev@gmail.com>
2025-11-09 16:20:51 +0300

    Update useSuppliers.ts

--- store/useSuppliers.ts
+++ store/useSuppliers.ts
@@ -1,1 +1,15 @@
+"use client";
+import { create } from "zustand";
+
+type SupplierPhones = Record<string, string>;
+
+type State = {
+  phones: SupplierPhones;
+  setPhones: (map: SupplierPhones) => void;
+};
+
+export const useSuppliers = create<State>((set) => ({
+  phones: {},
+  setPhones: (map) => set({ phones: map }),
+}));
 
